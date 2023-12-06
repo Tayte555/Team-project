0960203437c1901488f5eaf70205eeaead5229f1
Ishwar Saul <210173859@aston.ac.uk>
2023-12-06 03:10:23 +0000

    SQL code for inputing products to DB

--- store/src/functions.php
+++ store/src/functions.php
@@ -13,4 +13,15 @@ function login($connection){
     header("Location: login.php");
     die;
 }
+
+function insertProduct($connection, $productName, $brand, $price, $releaseDate, $stockQuantity, $category) {
+    $stmt = mysqli_prepare($connection, "INSERT INTO products (product_name, brand, price, release_date, stock_quantity, category) VALUES (?, ?, ?, ?, ?, ?)");
+    mysqli_stmt_bind_param($stmt, 'sssdii', $productName, $brand, $price, $releaseDate, $stockQuantity, $category);
+    if(mysqli_stmt_execute($stmt)) {
+        echo "Product inserted successfully.";
+    } else {
+        echo "ERROR: Could not execute query: " . mysqli_error($connection);
+    }
+    mysqli_stmt_close($stmt);
+}
 ?>
