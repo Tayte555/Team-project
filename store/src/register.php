9ddcf9f1ba1ad161de7b5c2f8763e3981e22ee10
Ishwar Saul <210173859@aston.ac.uk>
2023-12-10 03:46:09 +0000

    fixed register page typo

--- store/src/register.php
+++ store/src/register.php
@@ -182,7 +182,7 @@ session_start();
         </div>
   
         <div>
-          <button type="submit" class="flex w-full justify-center rounded-md bg-zinc-950 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-opacity-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-zinc-950 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-opacity-50 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign up</button>
         </div>
       </form>
   
