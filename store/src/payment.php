8f69aefb055ee0b9e5e336f0716a7f454c17a5bc
Ahmed-Majdi <aey.majdi@gmail.com>
2024-03-25 13:41:03 +0000

    Update payment.php
    new navi bar added to payment page

--- store/src/payment.php
+++ store/src/payment.php
@@ -16,74 +16,96 @@ session_start();
 </head>
 
 <body>
-  <!-- navbar -->
-  <header class="items-center bg-zinc-950 md:px">
-    <div class="flex flex-wrap place-items-center">
-    <section class="relative mx-auto">
-      <!-- navbar -->
-    <nav class="flex justify-between w-screen">
-      
-      <div class="px-2 flex w-full py-4 items-center">
-      
-        <a class="" href="index.php">
-        <!-- <img class="h-9" src="logo.png" alt="logo"> -->
-        <img class="h-6 
-         " src="./images/logowhite.png" alt="logo"/>         
-        </a>
+   <!-- navbar -->
+   <header class="items-center bg-zinc-950 md:px">
+        <div class="flex flex-wrap place-items-center">
+            <section class="relative mx-auto">
+                <!-- navbar -->
+                <nav class="flex justify-between w-screen">
 
-      <!-- Nav Links -->
-      <ul class="hidden md:flex mx-auto space-x-12 text-l text-white">
-        <li><a class="hover:text-gray-300" href="#">New Arrivals</a></li>
-        <li><a class="hover:text-gray-300" href="best_sellers.html">Best Sellers</a></li>
-        <li><a class="hover:text-gray-300" href="#">Sneakers                
-          <svg aria-hidden="true" class="w-5 inline-block origin-center rotate-90" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-arrow-right">
-          <path d="m9 18 6-6-6-6"/>
-          </svg>               
-        </a></li>
-        <li><a class="hover:text-gray-300" href="#">Apparel
-        <svg aria-hidden="true" class="w-5 inline-block origin-center rotate-90" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-arrow-right">
-          <path d="m9 18 6-6-6-6"/>
-        </svg>
-        </a></li>
-        <li><a class="hover:text-gray-300" href="#">Accessories
-        <svg aria-hidden="true" class="w-5 inline-block origin-center rotate-90" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-arrow-right">
-          <path d="m9 18 6-6-6-6"/>
-        </svg>
-        </a></li>
-        <li><a class="hover:text-gray-300 pr-40" href="./discover.html">Discover
-        <svg aria-hidden="true" class="w-5 inline-block origin-center rotate-90" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-arrow-right">
-          <path d="m9 18 6-6-6-6"/>
-        </svg>
-        </a></li>
-      </ul>
-      <!-- Header Icons -->
-      <div class="hidden xl:flex items-center -space-x-1 pr-6 text-gray-100">
-        <!-- search icon -->
-        <a class="hover:text-gray-300" href="#">
-        <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-search w-10">
-          <circle cx="11" cy="11" r="8"/>
-          <path d="m21 21-4.35-4.35"/>
-          </svg>
-        </a>
-        <!-- profile -->
-        <a class="flex items-center hover:text-gray-300 pr-1" href="login.php">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-user w-10 mx-auto"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle>
-        </svg>
-        
-        </a>
-        <!-- Cart      -->
-        <a class="flex items-center hover:text-gray-300" href="cart.php">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-cart w-10"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
-        </svg>
-        <!--<span class="flex absolute -mt-5 ml-4">
-          <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-            <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
-            </span>
-          </span> -->           
-        </a>             
-      </div>
-      </div>
-    </nav>
+                    <div class="px-2 flex w-full py-4 items-center">
+
+                        <a class="" href="index.php">
+                            <!-- <img class="h-9" src="logo.png" alt="logo"> -->
+                            <img class="h-6 
+				   " src="./images/logowhite.png" alt="logo" />
+                        </a>
+
+                        <!-- Nav Links -->
+                        <ul class="hidden md:flex mx-auto space-x-12 text-l text-white">
+                            <li><a class="hover:text-gray-300" href="newarrivals.php">New Arrivals</a></li>
+                            <li><a class="hover:text-gray-300" href="best_sellers.php">Best Sellers</a></li>
+                            <li><a class="hover:text-gray-300" href="sneakers.php">Sneakers
+                                    <svg aria-hidden="true" class="w-5 inline-block origin-center rotate-90"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="icon icon-arrow-right">
+                                        <path d="m9 18 6-6-6-6" />
+                                    </svg>
+                                </a></li>
+                            <li><a class="hover:text-gray-300" href="apparel.php">Apparel
+                                    <svg aria-hidden="true" class="w-5 inline-block origin-center rotate-90"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="icon icon-arrow-right">
+                                        <path d="m9 18 6-6-6-6" />
+                                    </svg>
+                                </a></li>
+                            <li><a class="hover:text-gray-300" href="accessories.php">Accessories
+                                    <svg aria-hidden="true" class="w-5 inline-block origin-center rotate-90"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="icon icon-arrow-right">
+                                        <path d="m9 18 6-6-6-6" />
+                                    </svg>
+                                </a></li>
+                            <li><a class="hover:text-gray-300 pr-40" href="./discover.php">Discover
+                                    <svg aria-hidden="true" class="w-5 inline-block origin-center rotate-90"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="icon icon-arrow-right">
+                                        <path d="m9 18 6-6-6-6" />
+                                    </svg>
+                                </a></li>
+                        </ul>
+                        <!-- Header Icons -->
+                        <div class="hidden xl:flex items-center -space-x-1 pr-6 text-gray-100">
+                            <!-- search icon -->
+                            <form action="search.php" method="GET" class="flex items-center">
+                                <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round" class="icon icon-search w-6 h-6">
+                                    <circle cx="11" cy="11" r="8" />
+                                    <path d="m21 21-4.35-4.35" />
+                                </svg>
+                                <div class="flex-grow">
+                                    <input type="search" name="search" placeholder="Search items..."
+                                        class="w-full h-10 px-4 bg-transparent text-white placeholder-gray-300 rounded transition-all duration-300 focus:border-blue-300 focus:bg-white focus:text-black" />
+                                </div>
+                            </form>
+                            <!-- profile -->
+                            <a class="flex items-center hover:text-gray-300 pr-1" href="login.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="icon icon-user w-10 mx-auto">
+                                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                    <circle cx="12" cy="7" r="4"></circle>
+                                </svg>
+
+                            </a>
+                            <!-- Cart      -->
+                            <a class="flex items-center hover:text-gray-300" href="cart.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="icon icon-cart w-10">
+                                    <circle cx="9" cy="21" r="1"></circle>
+                                    <circle cx="20" cy="21" r="1"></circle>
+                                    <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                                </svg>
+                                <!--<span class="flex absolute -mt-5 ml-4">
+						<span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+						  <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+						  </span>
+						</span> -->
+                            </a>
+                        </div>
+                    </div>
+                </nav>
     <main class="page payment-page">
         <section class="payment-form dark">
             <div class="container">
